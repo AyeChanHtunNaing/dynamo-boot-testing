4f21c47d647de3835bf331716172b510acc4ba81
AyeChanHtunNaing <naing340act@prestigein.com>
2024-09-03 19:37:19 +0700

    init : project

---
+++ src/main/java/dev/peacechan/DynamodbSpringbootApplication.java
@@ -0,0 +1,13 @@
+package dev.peacechan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamodbSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/peacechan/DynamodbSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.peacechan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
