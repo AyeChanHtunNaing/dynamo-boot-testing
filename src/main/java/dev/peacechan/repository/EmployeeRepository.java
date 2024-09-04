357bc46cb3be4b32f03db2ec60643fa6f8868513
AyeChanHtunNaing <naing340act@prestigein.com>
2024-09-04 09:09:50 +0700

    style : formatted to look better

--- src/main/java/dev/peacechan/repository/EmployeeRepository.java
+++ src/main/java/dev/peacechan/repository/EmployeeRepository.java
@@ -10,30 +10,28 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class EmployeeRepository {
+    @Autowired private DynamoDBMapper dynamoDBMapper;
 
-    @Autowired
-    private DynamoDBMapper dynamoDBMapper;
-
-    public Employee save(Employee employee){
+    public Employee save(Employee employee) {
         dynamoDBMapper.save(employee);
         return employee;
     }
 
-    public Employee getEmployeeById(String employeeId){
-        return dynamoDBMapper.load(Employee.class,employeeId);
+    public Employee getEmployeeById(String employeeId) {
+        return dynamoDBMapper.load(Employee.class, employeeId);
     }
 
-    public String delete(String employeeId){
-        Employee emp=dynamoDBMapper.load(Employee.class,employeeId);
+    public String delete(String employeeId) {
+        Employee emp = dynamoDBMapper.load(Employee.class, employeeId);
         dynamoDBMapper.delete(emp);
         return "Employee Deleted!";
     }
 
-    public String update(String employeeId, Employee employee){
+    public String update(String employeeId, Employee employee) {
         dynamoDBMapper.save(employee,
-                new DynamoDBSaveExpression().withExpectedEntry("employeeId",new ExpectedAttributeValue(
-                        new AttributeValue().withS(employeeId)
-                )));
+                new DynamoDBSaveExpression().withExpectedEntry("employeeId",
+                        new ExpectedAttributeValue(
+                                new AttributeValue().withS(employeeId))));
         return employeeId;
     }
 }
