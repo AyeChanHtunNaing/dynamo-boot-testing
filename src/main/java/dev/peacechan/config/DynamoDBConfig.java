a6fc777c81222df8675b006cdf4609ea166bc49a
AyeChanHtunNaing <naing340act@prestigein.com>
2024-09-04 08:38:32 +0700

    feat : config

---
+++ src/main/java/dev/peacechan/config/DynamoDBConfig.java
@@ -0,0 +1,27 @@
+package dev.peacechan.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfig {
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(buildAmazonDynamoDB());
+    }
+    private AmazonDynamoDB buildAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                        "dynamodb.ap-southeast-1.amazonaws.com", "ap-southeast-1"))
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials("AKIAYS2NU4MA2UNBPJEO",
+                                "yaKCJa2Yl59ACSEa/6GvzBABKKdfowbxiC+Xf4qQ")))
+                .build();
+    }
+}
